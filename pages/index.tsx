9a4c8991b2c1c0dbcee77ed42a59adc4eeb52629
Ting <tchangysl@gmail.com>
2022-11-06 22:25:41 +0800

    Init. Footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import Header from '../components/Header'
 import Jumbotron from '../components/Jumbotron'
 import AboutMe from '../components/AboutMe'
+import Projects from '../components/Projects'
+import Footer from '../components/Footer'
 
 export default function LandingPage() {
   return (
     <>
       <Header />
       <Jumbotron />
+      <Projects />
       <AboutMe />
+      <Footer />
     </>
   )
 }
