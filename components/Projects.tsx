db7c2a9a5c4e970a1ac1f7b146fb797c3efeb610
Ting <tchangysl@gmail.com>
2022-11-06 22:29:06 +0800

    Init. Projects

---
+++ components/Projects.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@chakra-ui/react'
+import SectionHeader from './SectionHeader'
+
+export default function Projects() {
+  return (
+    <Container as="section" p={6}>
+      <SectionHeader header="Projects" />
+    </Container>
+  )
+}
